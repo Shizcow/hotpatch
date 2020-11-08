9dff9184deb17ff46def710bf67f5604cc494143
Shizcow <pohl.devin@gmail.com>
2020-11-08 14:30:31 -0700

    Generic return type

--- src_bin/src/main.rs
+++ src_bin/src/main.rs
@@ -6,9 +6,15 @@ fn foo() -> i32 {
     0
 }
 
+#[patchable]
+fn bar() -> f64 {
+    1.0
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     foo(); // prints "I am from source"
     foo.hotpatch("../src_obj/target/debug/libsrc_obj.so")?;
     foo(); // prints something totally different
+    println!("{}", bar());
     Ok(())
 }
