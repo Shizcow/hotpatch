da3a31f93a2395b74a64aa07da0f16eeb25a3817
Shizcow <pohl.devin@gmail.com>
2021-02-01 20:33:32 -0700

    Rustfmt stuff

--- hotpatch_macros/src/lib.rs
+++ hotpatch_macros/src/lib.rs
@@ -3,9 +3,9 @@
 //! You probably want documentation for the [`hotpatch`](https://docs.rs/hotpatch) crate.
 
 use proc_macro::TokenStream;
-use syn::{ItemFn, parse::Nothing, Path};
-use std::sync::RwLock;
 use quote::ToTokens;
+use std::sync::RwLock;
+use syn::{parse::Nothing, ItemFn, Path};
 
 mod item_fn;
 
@@ -33,18 +33,18 @@ lazy_static::lazy_static! {
 pub fn patchable(attr: TokenStream, input: TokenStream) -> TokenStream {
     let modpath = get_modpath(attr);
     if modpath.is_err() {
-	return TokenStream::new();
+        return TokenStream::new();
     }
     if let Ok(fn_item) = syn::parse::<ItemFn>(input) {
-	item_fn::patchable(fn_item, modpath.unwrap())
+        item_fn::patchable(fn_item, modpath.unwrap())
     } else {
-	panic!("I can't hotpatch this yet!");
+        panic!("I can't hotpatch this yet!");
     }
 }
 
 /// Transforms a function into a [`HotpatchExport`](struct.HotpatchExport.html) capable of
 /// being exported and changing the behavior of a function in a seperate binary
-/// at runtime. **The original function is preserved.** 
+/// at runtime. **The original function is preserved.**
 ///
 /// Takes a single optional arguement: `modpath`. Used to spoof the module
 /// path.
@@ -63,29 +63,29 @@ pub fn patchable(attr: TokenStream, input: TokenStream) -> TokenStream {
 pub fn patch(attr: TokenStream, input: TokenStream) -> TokenStream {
     let modpath = get_modpath(attr);
     if modpath.is_err() {
-	return TokenStream::new();
+        return TokenStream::new();
     }
     if let Ok(fn_item) = syn::parse::<ItemFn>(input) {
-	item_fn::patch(fn_item, modpath.unwrap())
+        item_fn::patch(fn_item, modpath.unwrap())
     } else {
-	panic!("I can't patch this yet!");
+        panic!("I can't patch this yet!");
     }
 }
 
 fn get_modpath(attr: TokenStream) -> Result<Option<String>, ()> {
     if syn::parse::<Nothing>(attr.clone()).is_ok() {
-	Ok(None)
+        Ok(None)
     } else {
-	let path = syn::parse::<Path>(attr.clone());
-	if path.is_err() {
-	    proc_macro::Span::call_site().error("Expected module path")
+        let path = syn::parse::<Path>(attr.clone());
+        if path.is_err() {
+            proc_macro::Span::call_site().error("Expected module path")
 		.help("Just use #[patchable]; it's already module aware.")
 		.help("If you're trying to spoof a module path, the supplied arguement is an invalid path")
 		.emit();
-	    return Err(());
-	}
-	let mut ts = syn::export::TokenStream2::new();
-	path.unwrap().to_tokens(&mut ts);
-	Ok(Some(ts.to_string().replace(" ", "")))
+            return Err(());
+        }
+        let mut ts = syn::export::TokenStream2::new();
+        path.unwrap().to_tokens(&mut ts);
+        Ok(Some(ts.to_string().replace(" ", "")))
     }
 }
