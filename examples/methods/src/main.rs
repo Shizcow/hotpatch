52420a41d257fa79a902fb635f7f1fc9a3e3d5b5
Shizcow <pohl.devin@gmail.com>
2021-02-02 12:10:17 -0700

    Cleaned up things a lot; Self type should even work now

--- examples/methods/src/main.rs
+++ examples/methods/src/main.rs
@@ -2,31 +2,16 @@ use hotpatch::*;
 
 struct Foo {}
 // begin macro generation output
-#[allow(non_camel_case_types)]
-trait __HotpatchTraitGen_0 {
-    fn bar() -> ();
-}
-impl __HotpatchTraitGen_0 for Foo {
-    fn bar() {
-        println!("this is passthrough!");
-    }
-}
 impl Foo {
     #[cfg(not(doc))]
     #[allow(non_upper_case_globals)]
     pub const bar: hotpatch::MutConst<Patchable<dyn Fn() -> () + Send + Sync + 'static>> =
         hotpatch::MutConst::new(|| {
-            static __hotpatch_implgen_0: hotpatch::Patchable<
-                dyn Fn() -> () + Send + Sync + 'static,
-            > = hotpatch::Patchable::__new(|| {
-                hotpatch::Patchable::__new_internal(
-                    Box::new(<Foo as __HotpatchTraitGen_0>::bar)
-                        as Box<dyn Fn() -> () + Send + Sync + 'static>,
-                    "hw_bin::foo",
-                    "fn() -> ()",
-                )
-            });
-            &__hotpatch_implgen_0
+            #[patchable]
+            fn __hotpatch_staticwrap() {
+                println!("this is passthrough!");
+            }
+            &__hotpatch_staticwrap
         });
     #[cfg(doc)]
     /// Warnings here
